f2aa69100b2f83eb440370cb179033c09e4e8511
denniskaydalov <denniskaydalov@gmail.com>
2025-06-18 17:47:00 -0400

    Update errors and task flow

--- server.py
+++ server.py
@@ -14,6 +14,9 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+ICPC_BOT_CHANNEL = client.get_channel(os.getenv('ICPC_BOT_CHANNEL'))
+ADMIN_USER = client.get_user(os.getenv('ADMIN_USER'))
+
 def save_pickle(obj, filepath):
     with open(filepath, 'wb') as f:
         pickle.dump(obj, f)
@@ -49,9 +52,9 @@ def get_recent_problem_codeforces(handle):
                'from':1,
                'count':1 }
     try:
-        print('getting_request codeforces')
+        print('getting_request codeforces', handle)
         response = requests.get(url = URL, params = params, timeout=10)
-        print('got request codeforces')
+        print('got request codeforces', handle, response.json())
 
         recent_problem = response.json()['result'][0]
     
@@ -86,8 +89,23 @@ def get_clist_info(problem):
 
         return None
 
+def get_recent_problem_leetcode(handle):
+    URL = f'https://leetcode-api-pied.vercel.app/user/{handle}/submissions'
+
+    try:
+        print('getting_request leetcode', handle)
+        response = requests.get(url = URL, params = {'limit': 1}, timeout=10).json()[0]
+        print('got request leetcode', handle, response)
+
+        return Problem(name = response['title'],
+                       timestamp = response['timestamp'],
+                       ac = response['statusDisplay'] == 'Accepted',
+                       grader='leetcode')
+    except Exception as e:
+        print(e)
+        return None
+
 def rating_cf_to_lc(rating):
-    print(rating)
     return (   'Easy' if rating <= 1200 else
         'Medium' if rating <= 1700 else
         'Hard' if rating <= 2000 else
@@ -100,84 +118,90 @@ def rating_lc_to_cf(rating):
             2000 if rating == 'Hard+' else
             2000 + (200 * rating.count('+')) - 200)
 
-def get_recent_problem_leetcode(handle):
-    URL = f'https://leetcode-api-pied.vercel.app/user/{handle}/submissions'
-
-    try:
-        print('getting_request leetcode')
-        response = requests.get(url = URL, params = {'limit': 1}, timeout=10).json()[0]
-        print('got request leetcode')
-
-        return Problem(name = response['title'],
-                       timestamp = response['timestamp'],
-                       ac = response['statusDisplay'] == 'Accepted',
-                       grader='leetcode')
-    except Exception as e:
-        print(e)
-        return None
-
 def get_recent_problem(handle, grader):
     if grader == 'codeforces':
         return get_recent_problem_codeforces(handle)
     elif grader == 'leetcode':
         return get_recent_problem_leetcode(handle)
 
-def recent_problem_update():
-    updates = {  }
+def recent_problem_update(handle, grader):
+    update = None
 
-    for grader in saved_last_problems:
-        updates[grader] = {  }
+    last_problem = get_recent_problem(handle, grader)
 
-        for handle in saved_last_problems[grader]:
-            last_problem = get_recent_problem(handle, grader)
+    if last_problem and last_problem.timestamp != saved_last_problems[grader][handle].timestamp and last_problem.ac:
+        saved_last_problems[grader] = saved_last_problems.get(grader, {})
+        saved_last_problems[grader][handle] = last_problem
+        update = last_problem
+        save_pickle(saved_last_problems, SAVED_LAST_PROBLEMS_PATH)
 
-            if last_problem and last_problem.timestamp != saved_last_problems[grader][handle].timestamp and last_problem.ac:
-                saved_last_problems[grader] = saved_last_problems.get(grader, {})
-                saved_last_problems[grader][handle] = last_problem
-                updates[grader][handle] = last_problem
+    return update
 
-
-    save_pickle(saved_last_problems, SAVED_LAST_PROBLEMS_PATH)
-
-    return updates
-
-@tasks.loop(seconds=60)
+@tasks.loop(seconds=10)
 async def read_last_problem_loop():
-    recent_problems = recent_problem_update()
-    for grader in recent_problems:
-        for handle in recent_problems[grader]:
-            problem = recent_problems[grader][handle]
-            if get_clist_info(problem) and problem.rating and problem.url:
-                lc_rating = rating_cf_to_lc(problem.rating)
-                await send_message(f'<@{handle_to_user[grader][handle]}> solved [{problem.name} (difficulty: {problem.rating}/{lc_rating})]({problem.url}) on {grader}!')
-            else:
-                await send_message(f'<@{handle_to_user[grader][handle]}> solved {recent_problems[grader][handle].name} on {grader}!')
+    global ADMIN_USER
+    global ICPC_BOT_CHANNEL
+    global available_handles
+    global available_graders
 
-async def send_message(message, channel = None):
-    global __message__
+    try:
+        # cycle slowly through graders and handles
+        if available_handles:
+            if not available_graders:
+                handle = available_handles.pop(0)
+                if not available_handles:
+                    available_handles = []
+                    for grader in saved_last_problems:
+                        available_handles += saved_last_problems[grader]
+                    available_handles = list(set(available_handles))
+                handle = available_handles[0]
 
-    if not __message__ and channel:
-        __message__ = channel
+                available_graders = [grader for grader in saved_last_problems if handle in saved_last_problems[grader]]
 
-    message_channel = channel or __message__ or None
+            handle = available_handles[0]
+            grader = available_graders[0]
+            available_graders.pop(0)
 
-    print('seindg', message, 'with', message_channel)
-    if message_channel: 
-        await message_channel.channel.send(message)
+            problem = recent_problem_update(handle, grader)
+
+            if problem:
+                if not ICPC_BOT_CHANNEL:
+                    ICPC_BOT_CHANNEL = await client.fetch_channel(os.getenv('ICPC_BOT_CHANNEL'))
+
+                if ICPC_BOT_CHANNEL:
+                    if get_clist_info(problem) and problem.rating and problem.url:
+                        lc_rating = rating_cf_to_lc(problem.rating)
+                        await ICPC_BOT_CHANNEL.send(f'<@{handle_to_user[grader][handle]}> solved [{problem.name} (difficulty: {problem.rating}/{lc_rating})]({problem.url}) on {grader}!')
+                    else:
+                        await ICPC_BOT_CHANNEL.send(f'<@{handle_to_user[grader][handle]}> solved {problem.name} on {grader}!')
+    except Exception as e:
+        if not ADMIN_USER:
+            ADMIN_USER = await client.fetch_user(os.getenv('ADMIN_USER'))
+
+        if ADMIN_USER:
+            await ADMIN_USER.send(str(e)) # :(
 
 @client.event
 async def on_message(message):
-    global __message__ 
+    global ADMIN_USER
 
     if message.author == client.user:
         return
 
-    if message.content.startswith(f'{command_prefix}trackerbotinit'):
-        __message__ = message
-        await send_message('Init successful in this channel', message)
+    if message.content.startswith(f'{command_prefix}sendstate'):
+        if not ADMIN_USER:
+            ADMIN_USER = await client.fetch_user(os.getenv('ADMIN_USER'))
+
+        if ADMIN_USER:
+            await ADMIN_USER.send('ADMIN_USER: ' + str(ADMIN_USER) + '\n' +
+                                  'ICPC_BOT_CHANNEL: ' + str(ICPC_BOT_CHANNEL) + '\n' + 
+                                  'handle_to_user: ' + str(handle_to_user) + '\n' + 
+                                  'saved_last_problems: ' + str(saved_last_problems) + '\n' + 
+                                  'available_graders: ' + str(available_graders) + '\n' + 
+                                  'available_handles: ' + str(available_handles))
 
     if message.content.startswith(f'{command_prefix}help'):
-        await send_message('''
+        await message.channel.send('''
 Commands:
 ```
 !connect <platform> <username>              get notifications for problems you've solved
@@ -185,7 +209,7 @@ Commands:
 
 only codeforces or leetcode graders are currently supported
 ```
-                           ''', message)
+                           ''')
 
     if message.content.startswith(f'{command_prefix}connect'):
         splits = message.content.split(' ')
@@ -195,18 +219,20 @@ only codeforces or leetcode graders are currently supported
 
             if grader == "codeforces" or grader == "leetcode":
                 saved_last_problems[grader] = saved_last_problems.get(grader, {})
-                saved_last_problems[grader][handle] = Problem("", -1, True, "")#get_recent_problem(handle, grader)
+                saved_last_problems[grader][handle] = get_recent_problem(handle, grader)
 
                 handle_to_user[grader] = handle_to_user.get(grader, {})
                 handle_to_user[grader][handle] = message.author.id
 
+                available_handles.append(handle)
+
                 save_pickle(saved_last_problems, SAVED_LAST_PROBLEMS_PATH)
                 save_pickle(handle_to_user, HANDLE_TO_USER_PATH)
-                await send_message(f'Connected {handle} on {grader}', message)
+                await message.channel.send(f'Connected {handle} on {grader}')
             else:
-                await send_message(f'Must connect to codeforces or leetcode', message)
+                await message.channel.send(f'Must connect to codeforces or leetcode')
         else:
-            await send_message(f'Format for connect is `connect <grader> <handle>`.', message)
+            await message.channel.send(f'Format for connect is `connect <grader> <handle>`.')
 
     if message.content.startswith(f'{command_prefix}unconnect'):
         splits = message.content.split(' ')
@@ -220,15 +246,15 @@ only codeforces or leetcode graders are currently supported
                         save_pickle(saved_last_problems, SAVED_LAST_PROBLEMS_PATH)
                         save_pickle(handle_to_user, HANDLE_TO_USER_PATH)
 
-                        await send_message(f'Unconnected {handle} on {grader}', message)
+                        await message.channel.send(f'Unconnected {handle} on {grader}')
                     else:
-                        await send_message(f'Erorr unconnecting {handle}, database may be in a bad state', message)
+                        await message.channel.send(f'Erorr unconnecting {handle}, database may be in a bad state')
                 else:
-                    await send_message(f'{handle} does not exist in connections', message)
+                    await message.channel.send(f'{handle} does not exist in connections')
             else:
-                await send_message(f'Must unconnect to codeforces or leetcode', message)
+                await message.channel.send(f'Must unconnect to codeforces or leetcode')
         else:
-            await send_message(f'Format for unconnect is `unconnect <grader> <handle>`.', message)
+            await message.channel.send(f'Format for unconnect is `unconnect <grader> <handle>`.')
 
     if message.content.startswith(f'{command_prefix}leaderboard') or message.content.startswith(f'{command_prefix}lb'):
         pass
@@ -238,6 +264,12 @@ SAVED_LAST_PROBLEMS_PATH = 'saved_last_problems.pkl'
 HANDLE_TO_USER_PATH = 'handle_to_user.pkl'
 saved_last_problems = load_pickle(SAVED_LAST_PROBLEMS_PATH, { })
 handle_to_user = load_pickle(HANDLE_TO_USER_PATH, { })
+
+available_handles = []
+for grader in saved_last_problems:
+    available_handles += saved_last_problems[grader]
+available_handles = list(set(available_handles))
+available_graders = [grader for grader in saved_last_problems if available_handles[0] in saved_last_problems[grader]]
 
 __message__ = None
 command_prefix = "!"
